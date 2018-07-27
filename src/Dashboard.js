3e5d8d86cde56c0731c08a1cd48122ad96025edb
David Borah <dborah87@gmail.com>
2018-07-27 14:47:55 -0400

    Adding Logo

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -7,7 +7,7 @@ import './Dashboard.css';
 const Dashboard = (props) => (
     <div className="Dashboard-container">
       This is where the main TSF Selling Blitz Dashboard lives!
-      <img src="https://www.marchex.com/wp-content/uploads/2018/03/Call-Table-1024x430.png"/>
+      <img src="/TSF_Logo_White.png"/>
     </div>
 )
 
