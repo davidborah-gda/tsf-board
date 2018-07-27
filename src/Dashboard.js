5b9ba9d77f8538f6204ca7d1a9b6421a5ad8af47
David Borah <dborah87@gmail.com>
2018-07-27 14:54:49 -0400

    Logo

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -7,7 +7,7 @@ import './Dashboard.css';
 const Dashboard = (props) => (
     <div className="Dashboard-container">
       This is where the main TSF Selling Blitz Dashboard lives!
-      <img src="/TSF_Logo_White.png"/>
+      <img src="https://raw.githubusercontent.com/davidborah-gda/tsf-board/master/src/TSF_Logo_White.png" className="TSF-logo"/>
     </div>
 )
 
