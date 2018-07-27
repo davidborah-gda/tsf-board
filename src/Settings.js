5729cac09d9d160705ba5deaaa9f03a1d690c1cd
David Borah <dborah87@gmail.com>
2018-07-27 15:39:21 -0400

    Modified the settings page

--- src/Settings.js
+++ src/Settings.js
@@ -10,25 +10,58 @@ const Settings = (props) => (
     <legend>Choose your sales metrics settings!</legend>
 
     <div>
-        <input type="checkbox" id="scales" name="feature"
+        <input type="checkbox" id="calls" name="feature"
                value="scales" checked />
         <label for="scales">Calls Completed</label>
     </div>
 
     <div>
-        <input type="checkbox" id="horns" name="feature"
+        <input type="checkbox" id="DM_name" name="feature"
+               value="claws" />
+        <label for="claws">Decision Maker Name</label>
+    </div>
+
+    <div>
+        <input type="checkbox" id="DM_email" name="feature"
+               value="claws" />
+        <label for="claws">DM Email Address</label>
+    </div>
+
+     <div>
+        <input type="checkbox" id="meetings" name="feature"
                value="horns" />
         <label for="horns">Meetings Scheduled</label>
     </div>
 
+     <div>
+        <input type="checkbox" id="proposals" name="feature"
+               value="horns" />
+        <label for="horns">Proposals Sent</label>
+    </div>
+
     <div>
-        <input type="checkbox" id="claws" name="feature"
+        <input type="checkbox" id="Sales" name="feature"
                value="claws" />
         <label for="claws">Sales Completed</label>
     </div>
 
-</fieldset>
+    <div>
+        <input type="checkbox" id="social" name="feature"
+               value="claws" />
+        <label for="claws">Social Links</label>
+    </div>
 
+    <div>
+        <input type="checkbox" id="other" name="feature"
+               value="claws" />
+        <label for="claws">Other Success</label>
+    </div>
+
+    <div>
+        <input className="rename" id="custom" placeholder="(optional) Rename 'Other Success' metrics" />
+    </div>
+</fieldset>
+<button>Save Settings</button>
     </div>
 )
 
