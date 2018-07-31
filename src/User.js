be16e0fd8d349bf1d28efbbce6ef38646d7c13b9
David Borah <dborah87@gmail.com>
2018-07-31 18:13:18 -0400

    Fixed the Metrics cards to fit inside and space between inside the Mertrics container

--- src/User.js
+++ src/User.js
@@ -1,29 +1,34 @@
 /* eslint-disable */
 
-import React from 'react';
-import './User.css';
-import CallsCard from './CallsCard';
-import MetricsCard from './MetricsCard';
+import React from "react";
+import "./User.css";
+import CallsCard from "./CallsCard";
+import MetricsCard from "./MetricsCard";
 
-
-const User = (props) => (
-    <div>
+const User = props => (
+  <div className="Metrics-Main-Container">
     <div className="User-Topbar-container">
-    <img src="https://raw.githubusercontent.com/davidborah-gda/tsf-board/master/src/TSF_Logo_Black_MD.jpg" className="TSF-logo-SM" />
-    <h1 className="Username-title">John Smith</h1>
+      <img
+        src="https://raw.githubusercontent.com/davidborah-gda/tsf-board/master/src/TSF_Logo_Black_MD.jpg"
+        className="TSF-logo-SM"
+      />
+      <h1 className="Username-title">John Smith</h1>
     </div>
     <div className="User-container">
-    <CallsCard />
-    <div className="Metrics-container">
-    <MetricsCard />
-    <MetricsCard />
-    <MetricsCard />
-    <MetricsCard />
-    <MetricsCard />
-    <MetricsCard />
+      <CallsCard />
+      <div className="Metrics-container">
+        <MetricsCard />
+        <MetricsCard />
+        <MetricsCard />
+        <MetricsCard />
+        <MetricsCard />
+        <MetricsCard />
+        <MetricsCard />
+        <MetricsCard />
+        <MetricsCard />
+      </div>
     </div>
-    </div>
-    </div>
-)
+  </div>
+);
 
 export default User;
