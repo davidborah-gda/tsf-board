34b14ba65576baea0e48223074e90ce0d2e8a69f
David Borah <dborah87@gmail.com>
2018-07-31 09:31:38 -0400

    Added small black logo

--- src/User.js
+++ src/User.js
@@ -7,7 +7,7 @@ import './User.css';
 const User = (props) => (
     <div>
     <div className="User-Topbar-container">
-    <h3>Gravatar</h3>
+    <img src="https://raw.githubusercontent.com/davidborah-gda/tsf-board/master/src/TSF_Logo_Black_MD.jpg" className="TSF-logo" />
     <h1>John Smith</h1>
     </div>
     <div className="User-container">
