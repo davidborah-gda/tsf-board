df295094ec865b9145a1c4542ae02d569834c48c
David Borah <dborah87@gmail.com>
2018-07-31 09:20:46 -0400

    Added black logo

--- src/User.js
+++ src/User.js
@@ -5,12 +5,22 @@ import './User.css';
 
 
 const User = (props) => (
-    <div className="User-container">
-    <div className="Left">This is Main Call Volume Section.
-    <button>+</button>
-    <button>-</button>
+    <div>
+    <div className="User-Topbar-container">
+    <h3>Gravatar</h3>
+    <h1>John Smith</h1>
     </div>
-    <div className="Right">This is the Other Metrics Section.</div>
+    <div className="User-container">
+    <div className="Calls-container">
+    <h1>88</h1>
+    <button className="increase">+</button>
+    <button className="decrease">-</button>
+    <div className="TotalCalls-container">
+    <h1>99</h1>
+    </div>
+    </div>
+    <div className="Metrics-container">This is the Other Metrics Section.</div>
+    </div>
     </div>
 )
 
