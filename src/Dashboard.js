79a3ed5914d672b06ab138f8bbc1863b64bf6600
David Borah <dborah87@gmail.com>
2018-08-02 16:30:15 -0400

    Modified the dashboard

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -8,7 +8,17 @@ const Dashboard = (props) => (
     <div className="Dashboard-container">
       <img src="https://raw.githubusercontent.com/davidborah-gda/tsf-board/master/src/TSF_Logo_White.png" className="TSF-logo"/>
       <div className="Dashboard-Metrics-container">
-      
+      <div className="User-Row-container">
+      <h4 className="Test-User">User Name</h4>
+      <h4 className="Test-User">Total Calls</h4>
+      <h4 className="Test-User">DM Name</h4>
+      <h4 className="Test-User">DM Email</h4>
+      <h4 className="Test-User">Meetings Set</h4>
+      <h4 className="Test-User">Proposals Sent</h4>
+      <h4 className="Test-User">Social Links</h4>
+      <h4 className="Test-User">Other Success</h4>
+      <h4 className="Test-User">Sales Completed</h4>
+      </div>
       </div>
     </div>
 )
