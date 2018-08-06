7fa1860dae0d6000da25a2c03488f4aa3aee9a03
David Borah <dborah87@gmail.com>
2018-08-06 13:38:00 -0400

    More CSS modification

--- src/MetricsCard.js
+++ src/MetricsCard.js
@@ -10,8 +10,8 @@ const MetricsCard = (props) => (
     <p className="Total-calls-title1">Other Metric</p>
     <p className="Daily-calls1">55</p>
     <div className="Buttons-row1">
-    <button className="increase1">▲</button>
-    <button className="decrease1">▼</button>
+    <button className="increaseMet">▲</button>
+    <button className="decreaseMet">▼</button>
     <div className="TotalCalls-container1">
     <h1 className="Weekly-total-calls1">22</h1>
     </div>
