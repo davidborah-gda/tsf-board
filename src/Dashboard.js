2136407b78db15487c36e5bec36e28a88a40f173
David Borah <dborah87@gmail.com>
2018-08-06 17:29:26 -0400

    More CSS changes for the Dashboard

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -8,9 +8,15 @@ import DashboardRow from './DashboardRow';
 const Dashboard = (props) => (
     <div className="Dashboard-container">
     <div className="Event-Topbar">
-    <h4>Logo</h4>
-    <h4>Event Title</h4>
-    <h4>15:00</h4>
+    <div className="Topbar-logo-container">
+    <img src="https://raw.githubusercontent.com/davidborah-gda/tsf-board/master/src/TSF_Logo_Black_SM.jpeg" className="TSF-logo-sm"/>
+    </div>
+    <div className="Event-title-container">
+    <h2 className="Event-title">Event Title</h2>
+    </div>
+    <div className="Timer-container">
+    <h2 className="Timer-text">15:00</h2>
+    </div>
     </div>
       <img src="https://raw.githubusercontent.com/davidborah-gda/tsf-board/master/src/TSF_Logo_White.png" className="TSF-logo"/>
       <div className="Dashboard-Metrics-container">
