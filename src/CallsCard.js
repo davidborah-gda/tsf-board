bc82db55a69f30d339472714da7087c2dbecae09
David Borah <dborah87@gmail.com>
2018-08-07 11:18:20 -0400

    Was able to get the counter working but need to modify some CSS next

--- src/CallsCard.js
+++ src/CallsCard.js
@@ -1,27 +1,12 @@
 /* eslint-disable */
-
 import React from 'react';
 import './CallsCard.css';
+import Counter from './Counter';
+import './Counter.css';
 
 const CallsCard = (props) => (
     
-    
-    <div className="Calls-container">
-    <div className="Main-calls-container">
-    <h1 className="Total-calls-title">Calls Today</h1>
-    <div className="Daily-calls-container">
-    <h1 className="Daily-calls">88</h1>
-    </div>
-    </div>
-    <div className="Buttons-row">
-    <button className="increase">▲</button>
-    <button className="decrease">▼</button>
-    <div className="TotalCalls-container">
-    <h3 className="Weekly-title">Weekly Total</h3>
-    <h1 className="Weekly-total-calls">99</h1>
-    </div>
-    </div>
-    </div>
+    <Counter title="Calls Today" />
 )
 
 export default CallsCard;
