536ddc22ad641d8979d19040bf59f2a15e466f75
David Borah <dborah87@gmail.com>
2018-08-16 19:53:13 -0400

    removed basename process env. URL from router

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import { HashRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router>
         <App />
     </Router>,
 document.getElementById('root'));
