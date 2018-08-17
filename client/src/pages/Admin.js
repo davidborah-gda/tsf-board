cbe484f6f7cd96aacd286ef0a3540ef3f002cbfd
David Borah <dborah87@gmail.com>
2018-08-16 20:13:45 -0400

    Modified the forms style to center align the submit buttons

--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -17,6 +17,8 @@ setEventTitle(evt) {
       <h3>Time Clock Settings</h3>
       <input className="TimeClock-input" placeholder="Time in Minutes ex. 15"  />
       <button className="TSF-admin-button">Start Time Clock</button>
+      </form>
+      <form>
       <h4>Custom Event Title</h4>
       <input onChange={this.setEventTitle.bind(this)} className="Event-Title-input" placeholder="Event Title" />
     <button type="submit" className="TSF-admin-button">Save Event Title</button>
